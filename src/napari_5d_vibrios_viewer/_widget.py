4a1d9aa175ecbeee5605b29dc5a344ac866d3e6f
Niklas Netter <niknett@gmail.com>
2022-03-12 14:49:14 +0100

    works

--- src/napari_5d_vibrios_viewer/_widget.py
+++ src/napari_5d_vibrios_viewer/_widget.py
@@ -6,11 +6,35 @@ see: https://napari.org/plugins/stable/guides.html#widgets
 
 Replace code below according to your needs.
 """
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
-from magicgui import magic_factory
+import os
+from glob import glob
+
+import dask.array as da
+from dask import delayed
+from magicgui.widgets import FileEdit
+from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
+from skimage.io.collection import alphanumeric_key
+from tifffile import imread
 
 
-class ExampleQWidget(QWidget):
+def new_imread(f):
+    return imread(f)[1:, ...]
+
+
+def read_files(files):
+    filenames = sorted(glob(files), key=alphanumeric_key)
+    sample = new_imread(filenames[0])
+
+    lazy_imread = delayed(new_imread)
+    lazy_arrays = [lazy_imread(fn) for fn in filenames]
+    dask_arrays = [
+        da.from_delayed(delayed_reader, shape=sample.shape, dtype=sample.dtype)
+        for delayed_reader in lazy_arrays
+    ]
+    return da.stack(dask_arrays, axis=0)
+
+
+class DaskViewer(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # in one of two ways:
     # 1. use a parameter called `napari_viewer`, as done here
@@ -19,23 +43,23 @@ class ExampleQWidget(QWidget):
         super().__init__()
         self.viewer = napari_viewer
 
+        self.file_edit = FileEdit(label="Folder: ", mode="d")
         btn = QPushButton("Click me!")
         btn.clicked.connect(self._on_click)
 
         self.setLayout(QHBoxLayout())
+        self.layout().addWidget(self.file_edit.native)
         self.layout().addWidget(btn)
 
     def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        path = self.file_edit.value
+        channels = list(range(1, 4))
+        channel_names = ["mKate", "mKokappa", "GFP"]
+        channel_colormaps = ["red", "bop orange", "green"]
+        stacks = [read_files(os.path.join(path, f"*ch{i}*.tif")) for i in channels]
 
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
-
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+        for i in range(len(channels)):
+            print(stacks[i].shape)
+            self.viewer.add_image(
+                stacks[i], name=channel_names[i], colormap=channel_colormaps[i]
+            )
